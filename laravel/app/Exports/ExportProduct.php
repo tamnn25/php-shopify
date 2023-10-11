e8e441a09c28ebba61b69e7a8e23edc2f90bb8b8
tamnn <tam.nguyen@dssolution.jp>
2023-10-11 23:08:51 +0700

    Add header

--- laravel/app/Exports/ExportProduct.php
+++ laravel/app/Exports/ExportProduct.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExportProduct implements FromCollection
+class ExportProduct implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,25 @@ class ExportProduct implements FromCollection
     public function collection()
     {
         return Product::all();
+    }
+
+    public function headings(): array
+    {
+
+        return [
+            'stt',
+            'title',
+            'body_html',
+            'vendor',
+            'images',
+            'shopify_product_id',
+            'product_type',
+            'type_sync',
+            'time_sync',
+            'user_id',
+            'created_at',
+            'updated_at'
+        ];
+
     }
 }
